677da4e4d049952be3c5818831e6af251ea1e7bb
卓文城 <j095043@gmail.com>
2020-07-14 15:50:46 +0800

    Initial Commit

---
+++ SmallGames/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmallGames
+//
+//  Created by 卓文城 on 2020/7/14.
+//  Copyright © 2020 table. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
